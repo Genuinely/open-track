4a2bedb4111db15823f6344b11254606c42ed21f
jrfibel <jason.feibelman@gmail.com>
2023-12-09 20:18:50 -0500

    Pulling Airtable data in python

--- airtable.py
+++ airtable.py
@@ -1,9 +1,19 @@
 from pyairtable import Api
-import os
+from os import environ
+from dotenv import load_dotenv
+import pandas as pd
 
-api = Api(os.environ["AIRTABLE_API_KEY"])
-table = api.table("appSBKTKW43QcbpRf", "tblYS5wjMHpRd0WNa")
-table.all()
+def get_pd_dataframe():
+    api = Api(environ["AIRTABLE_API_KEY"])
+    table = api.table("appSBKTKW43QcbpRf", "tblYS5wjMHpRd0WNa")
+    data = table.all()
+    return pd.DataFrame.from_records((r['fields'] for r in data))
+
+if __name__ == "__main__":
+    load_dotenv()
+    print(get_pd_dataframe())
+
+
 # table.create({"Name": "Bob"})
 # table.update("recwAcQdqwe21asdf", {"Name": "Robert"})
 # table.delete("recwAcQdqwe21asdf")
