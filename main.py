9285823d9e18fdc015bcd1c26f4421907810b16e
haranrk <haranrajkumar97@gmail.com>
2023-12-09 19:15:59 -0500

    format files

--- main.py
+++ main.py
@@ -5,23 +5,26 @@ from concurrent.futures import ProcessPoolExecutor
 from multiprocessing import Manager, Process
 from dotenv import load_dotenv
 from time import sleep
+
 # import json
 
 load_dotenv()
 
+
 def now():
     return datetime.now().isoformat()
 
-def process_frame(img_buffer, lock, screenshot_folder):    
-    try: 
+
+def process_frame(img_buffer, lock, screenshot_folder):
+    try:
         screenshot_dt = now()
-        
-        print(f'[{now()}] Grabbing Screenshot')
+
+        print(f"[{now()}] Grabbing Screenshot")
         img = ImageGrab.grab(all_screens=True)
 
-        print(f'[{now()}] Saving Image')
-        filename = f'{screenshot_folder}/screenshot_{screenshot_dt}.jpg'
-        img.convert('RGB').save(filename, dpi=(100, 100), optimize=True)
+        print(f"[{now()}] Saving Image")
+        filename = f"{screenshot_folder}/screenshot_{screenshot_dt}.jpg"
+        img.convert("RGB").save(filename, dpi=(100, 100), optimize=True)
 
         if environ["IN_PROCESS_GPT"]:
             lock.acquire()
@@ -29,9 +32,10 @@ def process_frame(img_buffer, lock, screenshot_folder):
                 img_buffer.append(filename)
             finally:
                 lock.release()
-        
+
     except Exception as e:
-        print(f'[{now()}] Exception: {str(e)}')
+        print(f"[{now()}] Exception: {str(e)}")
+
 
 def submit_gpt4_vision_request(img_buffer):
     print("BATCH PROCESSING IMAGES : %s" % img_buffer)
@@ -39,6 +43,7 @@ def submit_gpt4_vision_request(img_buffer):
 
     # TODO - save GPT4 vision output in DB
     pass
+
 
 def run_gpt_ps(img_buffer, tmp_buffer, lock):
     ocr_interval = int(environ["OCR_INTERVAL"])
@@ -58,6 +63,7 @@ def run_gpt_ps(img_buffer, tmp_buffer, lock):
             while len(tmp_buffer) > 0:
                 tmp_buffer.pop()
 
+
 def main():
     max_workers = int(environ["MAX_WORKERS"])
     worker_sleep_duration = int(environ["SLEEP_DURATION"])
@@ -73,10 +79,11 @@ def main():
 
         with ProcessPoolExecutor(max_workers=max_workers) as executor:
             while True:
-                print(f'[{now()}] Starting thread')
+                print(f"[{now()}] Starting thread")
                 executor.submit(process_frame, img_buffer, lock, screenshot_folder)
-                print(f'[{now()}] Sleeping {worker_sleep_duration}s')
+                print(f"[{now()}] Sleeping {worker_sleep_duration}s")
                 sleep(worker_sleep_duration)
-  
-if __name__ == '__main__':
+
+
+if __name__ == "__main__":
     main()
--- understanding.py
+++ understanding.py
@@ -2,10 +2,12 @@ import base64
 import requests
 import os
 
+
 # Function to encode the image
 def encode_image(image_path):
-  with open(image_path, "rb") as image_file:
-    return base64.b64encode(image_file.read()).decode('utf-8')
+    with open(image_path, "rb") as image_file:
+        return base64.b64encode(image_file.read()).decode("utf-8")
+
 
 # Path to your image
 image_path = "computer_screenshot_heading.png"
@@ -14,32 +16,32 @@ image_path = "computer_screenshot_heading.png"
 base64_image = encode_image(image_path)
 
 headers = {
-  "Content-Type": "application/json",
-  "Authorization": f"Bearer {os.environ['OPENAI_KEY']}"
+    "Content-Type": "application/json",
+    "Authorization": f"Bearer {os.environ['OPENAI_KEY']}",
 }
 
 payload = {
-  "model": "gpt-4-vision-preview",
-  "messages": [
-    {
-      "role": "user",
-      "content": [
+    "model": "gpt-4-vision-preview",
+    "messages": [
         {
-          "type": "text",
-          "text": "This is a screenshot of my computer. What specific software program am I using? If I am using a browser, tell me the name of the browser and the name of the website."
-        },
-        {
-          "type": "image_url",
-          "image_url": {
-            "url": f"data:image/jpeg;base64,{base64_image}"
-          }
+            "role": "user",
+            "content": [
+                {
+                    "type": "text",
+                    "text": "This is a screenshot of my computer. What specific software program am I using? If I am using a browser, tell me the name of the browser and the name of the website.",
+                },
+                {
+                    "type": "image_url",
+                    "image_url": {"url": f"data:image/jpeg;base64,{base64_image}"},
+                },
+            ],
         }
-      ]
-    }
-  ],
-  "max_tokens": 300
+    ],
+    "max_tokens": 300,
 }
 
-response = requests.post("https://api.openai.com/v1/chat/completions", headers=headers, json=payload)
+response = requests.post(
+    "https://api.openai.com/v1/chat/completions", headers=headers, json=payload
+)
 
 print(response.json())
